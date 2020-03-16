08b5faf446cd177daee92ef835409819f2caf46e
damian1997 <damianveltkamp@Damians-MacBook-Pro.local>
2020-03-16 12:16:41 +0100

    Test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import { getForkers, getCommits, getIssues } from './src/scripts/api'
-import { cleanGithubData, sortCommits } from './src/scripts/data'
-import express from 'express'
+//import { getForkers, getCommits, getIssues } from './src/scripts/api'
+//import { cleanGithubData, sortCommits } from './src/scripts/data'
+//import express from 'express'
+const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/components/base/views')
@@ -14,23 +16,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/overview', async (req, res) => {
-  const apiBaseUrl = 'https://api.github.com/repos'
-  const split_string = req.query.repository.split('https://github.com/')	
-  const finalstring = split_string[1].split('/')
-  const fetchUrl = `${apiBaseUrl}/${finalstring[0]}/${finalstring[1]}`
+  //const apiBaseUrl = 'https://api.github.com/repos'
+  //const split_string = req.query.repository.split('https://github.com/')	
+  //const finalstring = split_string[1].split('/')
+  //const fetchUrl = `${apiBaseUrl}/${finalstring[0]}/${finalstring[1]}`
 
-  const forkers = await getForkers(apiBaseUrl,finalstring[0],finalstring[1])
-  .then(async (entrys) => {
-  return await getCommits(apiBaseUrl,entrys)
-  })
+  //const forkers = await getForkers(apiBaseUrl,finalstring[0],finalstring[1])
+  //.then(async (entrys) => {
+  //return await getCommits(apiBaseUrl,entrys)
+  //})
 
-  const cleanedForkers = await cleanGithubData(forkers)
-  .then(async (entrys) => {
-  return await sortCommits(entrys)
-  })
+  //const cleanedForkers = await cleanGithubData(forkers)
+  //.then(async (entrys) => {
+  //return await sortCommits(entrys)
+  //})
+  
+  const cleanedForkers = ''
 
   res.render(`${__dirname}/src/components/overview/views/overview`, {
-    title: 'Home',
+    title: 'Overview',
     basePartialsPath: `${__dirname}/src/components/base/views/partials`,
     forkers: cleanedForkers
   })
@@ -38,12 +42,16 @@ app.get('/overview', async (req, res) => {
 
 app.get('/detail/:id', (req, res) => {
   console.log(req.params.id, '  Heeey')
+  res.render(`${__dirname}/src/components/detail/views/detail`, {
+    title: 'Detail',
+    basePartialsPath: `${__dirname}/src/components/base/views/partials`,
+  })
 })
 
 app.get('/detail', (req, res) => {
   console.log('  Heeey')
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('App has booted on port 3000')
 })
