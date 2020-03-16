7ab00c798a1e212944b6eb4f41243a4120d58c1f
damian1997 <damianveltkamp@Damians-MacBook-Pro.local>
2020-03-16 12:18:01 +0100

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //import express from 'express'
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/components/base/views')
